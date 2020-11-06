c06a7dab210d3e6f9b534e093b1d37d36b0e5df9
hemanth kumar <pusupalahemanthkumar@gmail.com>
2020-11-06 20:01:32 +0530

    02-navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+// Importing Required Files And Packages Here.
 import React from "react";
 
+import Header from "./components/Header/Header";
+
+// Defining Functional Component Here.
 function App() {
   return (
     <div className="App">
-      Welcome To ReactBootstarp Tutorials.
+      <Header />
+      
     </div>
   );
 }
